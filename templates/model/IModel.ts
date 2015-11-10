22bba6e486554dfed2c4025d74405279af4d87a1
hirsch88 <gery.hirschfeld@w3tec.ch>
2015-11-10 15:23:12 +0100

    fix(models): fixes ts-lint errors

--- templates/model/IModel.ts
+++ templates/model/IModel.ts
@@ -17,53 +17,53 @@ module app.<%= namespace %> {
    * This will be used to create or load models from the backend. Moreover
    * static variables are used to store global configs of the models
    */
-  export interface I<%= capitalizedName %>ModelStatic extends IBaseStaticModel {
+  export interface I<%= capitalizedName %>ModelStatic {
     /**
-     * Gives the model a Independent and Identically Distributed name
-     */
+    * Gives the model a Independent and Identically Distributed name
+    */
     IID: string;
     /**
-     * Defines the events for this model
-     */
+    * Defines the events for this model
+    */
     EVENTS: I<%= capitalizedName %>ModelEvents;
     /**
-     * This static variable stores the defaults
-     */
+    * This static variable stores the defaults
+    */
     DEFAULTS: I<%= capitalizedName %>ModelBackend;
     /**
-     * Creates and returns a new Model instance
-     */
+    * Creates and returns a new Model instance
+    */
     new (any): I<%= capitalizedName %>Model;
   }
 
   /**
-   * @name I<%= capitalizedName %>Model
-   * @description
-	 * Defines the data structure of the model and
-   * some behaviour methods
-	 */
-	export interface I<%= capitalizedName %>Model {<% for (var i = 0, p = modelProperties.length; i < p; i++) { %>
-		<%= modelProperties[i].inAppName %>?: <%= modelProperties[i].type %>;<% } %>
+  * @name I<%= capitalizedName %>Model
+  * @description
+  * Defines the data structure of the model and
+  * some behaviour methods
+  */
+  export interface I<%= capitalizedName %>Model {<% for (var i = 0, p = modelProperties.length; i < p; i++) { %>
+    <%= modelProperties[i].inAppName %>?: <%= modelProperties[i].type %>;<% } %>
 
-    save(): Promise<I<%= capitalizedName %>Model>;
-		destroy(): Promise<any>;
+    save(): Promise <I<%= capitalizedName %>Model>;
+    destroy(): Promise<any>;
   }
 
   /**
-   * @name I<%= capitalizedName %>ModelBackend
-   * @description
-	 * Defines the backend structure
-	 */
-	export interface I<%= capitalizedName %>ModelBackend {<% for (var i = 0, p = modelProperties.length; i < p; i++) { %>
-		<%= modelProperties[i].inApiName %>?: <% if (modelProperties[i].type === 'moment.Moment') { %>string <% } else {%><%=modelProperties[i].type %>;<% } %><% } %>
-	}
+  * @name I<%= capitalizedName %>ModelBackend
+  * @description
+  * Defines the backend structure
+  */
+  export interface I<%= capitalizedName %>ModelBackend {<% for (var i = 0, p = modelProperties.length; i < p; i++) { %>
+    <%= modelProperties[i].inApiName %>?: <% if (modelProperties[i].type === 'moment.Moment') { %>string;<% } else {%><%=modelProperties[i].type %>;<% } %><% } %>
+  }
 
   /**
-   * @name I<%= capitalizedName %>ModelEvents
-   * @description
-   * This interface defines the model events which can
-   * be listend at
-   */
+  * @name I<%= capitalizedName %>ModelEvents
+  * @description
+  * This interface defines the model events which can
+  * be listend at
+  */
   export interface I<%= capitalizedName %>ModelEvents {
     CREATED: string;
     UPDATED: string;
@@ -71,14 +71,14 @@ module app.<%= namespace %> {
   }
 
   /**
-   * @name I<%= capitalizedName %>ModelFactory
-   * @description
-	 * Defines the model factory. This is used to access the model
-   * throw the angular system
-	 */
+  * @name I<%= capitalizedName %>ModelFactory
+  * @description
+  * Defines the model factory. This is used to access the model
+  * throw the angular system
+  */
   export interface I<%= capitalizedName %>ModelFactory {
-		getModel(): I<%= capitalizedName %>ModelStatic;
-		create(data?): I<%= capitalizedName %>Model;
+      getModel(): I<%= capitalizedName %>ModelStatic;
+      create(data ?): I<%= capitalizedName %>Model;
   }
 
 }
