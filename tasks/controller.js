10765a70e64c0a97fbcb9c5ea78a90ae2690e997
hirsch88 <gery.hirschfeld@w3tec.ch>
2015-11-11 07:46:05 +0100

    feat(controller): adds new controller generator

--- tasks/controller.js
+++ tasks/controller.js
@@ -6,41 +6,58 @@
  */
 'use strict';
 
-var gulp     = require('gulp'),
-    conflict = require('gulp-conflict'),
-    template = require('gulp-template'),
-    rename   = require('gulp-rename'),
-    _        = require('underscore.string'),
-    inquirer = require('inquirer'),
-    path     = require('path');
+var gulp = require('gulp'),
+  conflict = require('gulp-conflict'),
+  template = require('gulp-template'),
+  rename = require('gulp-rename'),
+  _ = require('underscore.string'),
+  inquirer = require('inquirer'),
+  path = require('path');
 
 var util = require('../util.js');
 
 gulp.task('ctrl', ['controller']);
 gulp.task('controller', function (done) {
-  var prompts = [{
-    name: 'name',
-    message: 'What is the name of your controller?'
-  }];
+  var prompts = [
+    {
+      name: 'name',
+      message: 'What is the name of your controller?'
+    }, {
+      name: 'description',
+      message: 'Please describe your new controller:'
+    }
+  ];
   //Ask
   inquirer.prompt(prompts, function (answers) {
     util.modulePrompt(function (module) {
-      util.folderPrompt('controllers', function (folder) {
-        var p = util.getPaths('controller', ['ts']);
-        var context = util.buildContext([answers, folder]);
-        var fileName = context.camelizedName + '.controller.ts';
-        var target = path.join(p.target, module, context.path);
-        context.module = module;
-        gulp.src(p.source)
-          .pipe(template(context))
-          .pipe(rename(fileName))
-          .pipe(conflict(target))
-          .pipe(gulp.dest(target))
-          .on('end', function () {
+      util.folderPrompt(module, function (folder) {
+        util.getGitConfig(function (gitConfig) {
+
+          // Build Context and Paths
+          var p = util.getPaths('controller', ['ts']);
+          var context = util.buildContext([answers, folder, gitConfig]);
+          var target = path.join(p.target, 'modules', context.path);
+          // Generates the controller
+          generateController(target, context, function () {
             done();
-            util.onSuccess('Controller', path.join(target, fileName));
           });
+
+        });
       });
     });
   });
 });
+
+function generateController(target, context, done) {
+  var fileName = context.capitalizedName + 'Controller.ts';
+  var sourceClass = path.join(__dirname, '../templates/controller/Controller.ts');
+  gulp.src(sourceClass)
+    .pipe(template(context))
+    .pipe(rename(fileName))
+    .pipe(conflict(target))
+    .pipe(gulp.dest(target))
+    .on('end', function () {
+      util.onSuccess('Controller', path.join(target, fileName));
+      done();
+    });
+}
